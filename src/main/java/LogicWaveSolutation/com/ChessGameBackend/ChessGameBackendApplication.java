2e01847c798f62a96dc40e161fe02f8079ef6d82
saikat112 <saikatmanna112@gmail.com>
2024-10-28 15:47:09 +0530

    Initial commit

---
+++ src/main/java/LogicWaveSolutation/com/ChessGameBackend/ChessGameBackendApplication.java
@@ -0,0 +1,13 @@
+package LogicWaveSolutation.com.ChessGameBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessGameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessGameBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LogicWaveSolutation/com/ChessGameBackend/ChessGameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package LogicWaveSolutation.com.ChessGameBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessGameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
